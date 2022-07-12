9e5f884a070e7244403c23de7d33cc00a71169b3
Oleg S <leshiyoleg@mail.ru>
2022-07-12 23:04:49 +0300

    Added tasks and files for Lection_2

---
+++ function_file.py
@@ -0,0 +1,8 @@
+# new file function_file.py
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
--- lec.py
+++ lec.py
@@ -179,20 +179,20 @@
 # print(colors)
 
 # Функции
-def f(x):
-    return x**2
+# def f(x):
+#     return x**2
 
-def f(x):
-    if x == 1:
-        return 'Целое'
-    elif x == 2.3:
-        return 23
-    else:
-        return
-        
-print(f(1)) # Целое
-print(f(2.3)) # 23
-print(f(28)) # None
-print(type(f(1))) # str
-print(type(f(2.3))) # int
-print(type(f(28))) # NoneType
+# def f(x):
+#     if x == 1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return
+
+# print(f(1)) # Целое
+# print(f(2.3)) # 23
+# print(f(28)) # None
+# print(type(f(1))) # str
+# print(type(f(2.3))) # int
+# print(type(f(28))) # NoneType

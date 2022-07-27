94eb2ed227418ba0f174fd33a470a89187aa78c9
Oleg S <leshiyoleg@mail.ru>
2022-07-27 20:16:51 +0300

    Added tasks and files for Lection_4

---
+++ Lection_3.py
@@ -0,0 +1,15 @@
+# Написать программу сложения двух
+# чисел
+
+# x = int(input('x = '))
+# y = int(input('y = '))
+# print(f'{x} + {y} = {x + y}')
+
+# def sum(a, b):
+#     return a + b
+
+# print(sum(4,5))
+
+# sum = lambda a, b: a + b
+# print (sum(4,5))
+
